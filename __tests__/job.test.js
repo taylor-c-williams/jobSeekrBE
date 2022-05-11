ec8c5fcca27bfeec1d462013cea82bc6daa7686e
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-05-10 22:32:03 -0700

    delete job test passing

--- __tests__/job.test.js
+++ __tests__/job.test.js
@@ -6,7 +6,7 @@ const UserService = require('../lib/services/UserService');
 
 const agent = request.agent(app);
 
-// Mock Job Object
+// Mock Job Objects
 const mockJob = {
   fav: false,
   remote: 'hybrid',
@@ -21,6 +21,22 @@ const mockJob = {
   description: 'A cool job',
   notes: 'Alanis Morissette is canadian',
   contact: 'Alanis (208) 772-5518'
+};
+
+const mockJob2 = {
+  fav: false,
+  remote: 'hybrid',
+  zipcode: 12345,
+  applied: true,
+  phone_screen: true,
+  take_home: true,
+  offer: false,
+  rejected: false,
+  accepted: false,
+  url: 'https://taylorcallanwilliams.io',
+  description: 'A cool job',
+  notes: 'Taylor is Cool',
+  contact: 'hello@taylorcallanwilliams.io'
 };
 
 // Dummy user object
@@ -39,10 +55,16 @@ describe ('Job route tests', () => {
     pool.end();
   });
 
+  // Test: Auth + Add new Job
   it('allows a logged in user to add a new job', async () => {
+    
+    // Create new user & log in
     const user = await UserService.create({ ...mockUser });
     await agent.post('/api/v1/users/login').send({ username: user.username, password: mockUser.password });
+    
+    // Post a new Job
     const res = await agent.post('/api/v1/jobs').send({ ...mockJob, user_id: user.id });
+    
     expect(res.body).toEqual({
       id: expect.any(String),
       created_at: expect.any(String),
@@ -50,6 +72,57 @@ describe ('Job route tests', () => {
       user_id: expect.any(String),
       ...mockJob,
     });
+  });
+
+  // Test: Auth + get all user's jobs
+  it('allows a logged in user to get all of their jobs', async () => {
+
+    // Create user & log in
+    const user = await UserService.create({ ...mockUser });
+    await agent.post('/api/v1/users/login').send({ username: user.username, password: mockUser.password });
+
+    // Post 2 jobs
+    await agent.post('/api/v1/jobs').send({ ...mockJob, user_id: user.id });
+    await agent.post('/api/v1/jobs').send({ ...mockJob2, user_id: user.id });
+
+    const res = await agent.get('/api/v1/jobs');
+    expect(res.body).toEqual([{
+      id: expect.any(String),
+      created_at: expect.any(String),
+      last_updated: expect.any(String),
+      user_id: expect.any(String),
+      ...mockJob,
+    },
+    {
+      id: expect.any(String),
+      created_at: expect.any(String),
+      last_updated: expect.any(String),
+      user_id: expect.any(String),
+      ...mockJob2,
+    }  
+    ]);
+  });
+
+  // Test: Auth + Delete Job
+  it('allows a logged in user to delete job', async () => {
+
+    // Create user & log in
+    const user = await UserService.create({ ...mockUser });
+    await agent.post('/api/v1/users/login').send({ username: user.username, password: mockUser.password });
+
+    // Post 2 jobs
+    await agent.post('/api/v1/jobs').send({ ...mockJob, user_id: user.id });
+    await agent.post('/api/v1/jobs').send({ ...mockJob2, user_id: user.id });
+
+    await agent.delete('/api/v1/jobs/1');
+    const res = await agent.get('/api/v1/jobs');
+    expect(res.body).toEqual([{
+      id: expect.any(String),
+      created_at: expect.any(String),
+      last_updated: expect.any(String),
+      user_id: expect.any(String),
+      ...mockJob2,
+    }]);
   });
 
 });
