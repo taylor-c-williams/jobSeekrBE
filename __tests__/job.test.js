2abe3b3ddba7651162cc6d9f9427a25a1839729e
Taylor Callan Williams <taylor.callan.williams@gmail.com>
2022-05-11 20:25:43 -0700

    update jobs endpoint, test drafted

--- __tests__/job.test.js
+++ __tests__/job.test.js
@@ -21,7 +21,7 @@ const mockJob = {
   url: 'http://www.glassdoor.com/',
   description: 'A cool job',
   notes: 'Alanis Morissette is canadian',
-  contact: 'Alanis (208) 772-5518'
+  contact: 'Alanis (208) 772-5555'
 };
 
 const mockJob2 = {
@@ -46,8 +46,9 @@ const mockUser = {
   password: '12345'
 };
 
-// Create User & Log In
+// Create User & Log In (Delete all test jobs first)
 const mockUserLogin = async () => { 
+  await deleteAllJobs();
   const user = await UserService.create({ ...mockUser });
   await agent.post('/api/v1/users/login').send({ username: user.username, password: mockUser.password });
   return user;
@@ -85,9 +86,8 @@ describe ('Job route tests', () => {
     });
   });
 
-  // Test: Auth + Get All user's jobs
+  // Test: Auth + Get All User's jobs
   it('allows a logged in user to get all of their jobs', async () => {
-    await deleteAllJobs();
     const user = await mockUserLogin();
     await post2Jobs(user.id);
 
@@ -111,7 +111,6 @@ describe ('Job route tests', () => {
 
   // Test: Auth + Delete Job
   it('allows a logged in user to delete job', async () => {
-    await deleteAllJobs();
     const user = await mockUserLogin();
     await post2Jobs(user.id);
 
@@ -128,7 +127,6 @@ describe ('Job route tests', () => {
 
   // Test: Auth + Get Job By ID
   it('allows a logged in user to get a job by ID', async () => {
-    await deleteAllJobs();
     const user = await mockUserLogin();
     await post2Jobs(user.id);
 
@@ -141,6 +139,15 @@ describe ('Job route tests', () => {
       ...mockJob,
     }
     ]);
+  });
+
+  // Test: Auth + Update Job
+  it('allows a logged in user to update a job', async () => {
+    const user = await mockUserLogin();
+    await post2Jobs(user.id);
+
+    const res = await agent.patch('/api/v1/jobs/1').send({ fav: true });
+    expect(res.body).toEqual({ ...mockJob, fav: true }); 
   });
 
 });
